ff858c9ff0622071375ab8f1bb692e57d2d92993
Chad Fowler <chad@chadfowler.com>
2010-01-28 16:27:16 -0700

    added test project and sample test

---
+++ HTTPartyTests/HTTPartyTest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace HTTPartyTests
+{
+    [TestFixture]
+    public class HTTPartyTest
+    {
+        [Test]
+        public void CanCallGetWithURL() 
+        {
+            Assert.AreEqual(1, 2);
+        }
+    }
+}
